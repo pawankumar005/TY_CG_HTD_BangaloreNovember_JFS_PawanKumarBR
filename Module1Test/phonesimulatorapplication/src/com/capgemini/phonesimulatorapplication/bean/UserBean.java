c277cfd791341ccd2d6772f8a4fae11ce2d1fbfe
pawankumar005 <pavan.570818@gmail.com>
2019-12-13 13:13:56 +0530

    Module1 test

---
+++ Module1Test/phonesimulatorapplication/src/com/capgemini/phonesimulatorapplication/bean/UserBean.java
@@ -0,0 +1,12 @@
+package com.capgemini.phonesimulatorapplication.bean;
+
+import lombok.Data;
+
+@Data
+public class UserBean {
+	private String  name;
+	private  long number;
+	private String group;
+
+
+}
